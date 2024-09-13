5115e47742005c357339ee9d81fe84f10b0b7043
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-09-13 19:43:59 -0400

    one last alert added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+alert('Click on the generated password to copy it')
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
     "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
