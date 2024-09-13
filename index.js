253d47e2c9cf171936058f3edac337319e14c1cd
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2024-09-13 19:34:44 -0400

    done with the project

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
     "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
     "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
+    "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+
+const fullCharacters = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e",
+    "f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w",
     "x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!",
     "@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",",
     "|",":",";","<",">",".","?",
@@ -15,7 +20,14 @@ const charactersDiv = document.querySelector('#characters-div')
 const mode = document.getElementById('mode-switch')
 const specialcharacters = document.getElementById('specialcharacters')
 
+const mainButton = document.querySelector('#main-button')
 
+const password1Holder = document.querySelector('#password-1')
+const password2Holder = document.querySelector('#password-2')
+
+let passwordLength = 12
+let password1 = ''
+let password2 = ''
 let isLightMode = false
 let isUsingSpecialCharacters = false
 
@@ -67,3 +79,45 @@ function handleModeSwitch() {
         charactersDiv.classList.add('dark-characters')
     }
 }
+
+//Generating password
+mainButton.addEventListener('click',generatePassword)
+
+function generatePassword(){
+    if (isUsingSpecialCharacters){
+        for(let i=1;i<=passwordLength; i++){
+            let index = Math.floor(Math.random()*fullCharacters.length)
+            password1 += `${fullCharacters[index]}`
+        }
+        for(let i=1;i<=passwordLength; i++){
+            let index = Math.floor(Math.random()*fullCharacters.length)
+            password2 += `${fullCharacters[index]}`
+        }
+    }else{
+        for(let i=1;i<=passwordLength; i++){
+            let index = Math.floor(Math.random()*characters.length)
+            password1 += `${characters[index]}`
+        }
+        for(let i=1;i<=passwordLength; i++){
+            let index = Math.floor(Math.random()*characters.length)
+            password2 += `${characters[index]}`
+        }
+    }
+    password1Holder.value = password1
+    password2Holder.value = password2
+    password1=''
+    password2=''
+}
+
+password1Holder.addEventListener('click',copyToClipboard)
+password2Holder.addEventListener('click',copyToClipboard)
+
+function copyToClipboard(e) {
+    if(e.target.value){
+        navigator.clipboard.writeText(e.target.value)
+
+    alert('Your password was copied to clipboard')
+    }
+}
+
+//Copying to clipboard
